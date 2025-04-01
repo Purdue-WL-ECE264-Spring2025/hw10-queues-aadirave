c7f5344e80e3165b3a66756f050b19eb22eadabe
Aadi Rave <arave@purdue.edu>
2025-04-01 12:04:57 -0400

    finished

--- linked_list.c
+++ linked_list.c
@@ -1,18 +1,90 @@
 #include "linked_list.h"
 
+#include <stddef.h>
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) {
+  struct list_node *nnode = calloc(1, sizeof(struct list_node));
+  if (nnode == NULL) {
+    return NULL;
+  }
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  nnode->value = value;
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+  return nnode;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *head = list->head;
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  struct list_node *new_head = new_node(value);
+  if (new_head == NULL) {
+    return;
+  }
 
-void free_list(struct linked_list list) {}
+  new_head->next = head;
+  list->head = new_head;
+}
+
+void insert_at_tail(struct linked_list *list, size_t value) {
+  if (list->head == NULL) {
+    list->head = new_node(value);
+
+    return;
+  }
+
+  struct list_node *curr = list->head;
+  while (curr->next != NULL) {
+    curr = curr->next;
+  };
+
+  curr->next = new_node(value);
+}
+
+size_t remove_from_head(struct linked_list *list) {
+  struct list_node *head = list->head;
+
+  size_t ret_val = head->value;
+  list->head = head->next;
+
+  free(head);
+
+  return ret_val;
+}
+
+size_t remove_from_tail(struct linked_list *list) {
+  struct list_node *head = list->head;
+  struct list_node *curr = head, *prev = NULL;
+
+  while (curr->next != NULL) {
+    prev = curr;
+    curr = curr->next;
+  }
+
+  if (prev == NULL) {
+    // first node
+    return remove_from_head(list);
+  }
+
+  prev->next = NULL;
+  size_t to_ret = curr->value;
+
+  free(curr);
+
+  return to_ret;
+}
+
+void free_list(struct linked_list list) {
+  struct list_node *head = list.head;
+  struct list_node *curr = head;
+
+  while (curr != NULL) {
+    struct list_node *next = curr->next;
+    free(curr);
+
+    curr = next;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,78 @@
 #include "queue.h"
+#include "linked_list.h"
 #include "tile_game.h"
+#include <stdbool.h>
+#include <stdio.h>
 
-void enqueue(struct queue *q, struct game_state state) {}
+#define BOARD_SIZE 4
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+void enqueue(struct queue *q, struct game_state state) {
+  insert_at_tail(&q->data, serialize(state));
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+struct game_state dequeue(struct queue *q) {
+  size_t init = remove_from_head(&(q->data));
+
+  return deserialize(init);
+}
+
+bool is_finished(struct game_state board) {
+  if (board.tiles[3][3] != 0) {
+    return false;
+  }
+
+  int expected = 1;
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    for (int j = 0; j < BOARD_SIZE; j++) {
+      if (i == 3 && j == 3) {
+        continue;
+      }
+
+      if (board.tiles[i][j] != expected++) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+int number_of_moves(struct game_state start) {
+  struct queue q = {.data = {.head = NULL}};
+  enqueue(&q, start);
+
+  while (q.data.head != NULL) {
+    struct game_state cur_state = dequeue(&q);
+    if (is_finished(cur_state)) {
+      free_list(q.data);
+      return cur_state.num_steps;
+    }
+
+    if (cur_state.empty_row != 0) {
+      struct game_state new_state = cur_state;
+      move_down(&new_state);
+      enqueue(&q, new_state);
+    }
+
+    if (cur_state.empty_row != BOARD_SIZE - 1) {
+      struct game_state new_state = cur_state;
+      move_up(&new_state);
+      enqueue(&q, new_state);
+    }
+
+    if (cur_state.empty_col != 0) {
+      struct game_state new_state = cur_state;
+      move_right(&new_state);
+      enqueue(&q, new_state);
+    }
+
+    if (cur_state.empty_col != BOARD_SIZE - 1) {
+      struct game_state new_state = cur_state;
+      move_left(&new_state);
+      enqueue(&q, new_state);
+    }
+  }
+
+  free_list(q.data);
+  return -1;
+}
